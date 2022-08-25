c00394ef36efdd1d15696094e3349a478ac530a1
Kwesi Hervie <herviek2001@gmail.com>
2022-08-25 13:37:54 +0100

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        'sans': ['Barlow Condensed', ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: [],
+}
